7130eef47c87f90ff742361554bc0ebf9a760069
Your Name <you@example.com>
2024-10-18 18:22:49 +0300

    create type adabter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,17 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+// the name should be as the name of the class
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel({
@@ -11,3 +21,7 @@ class NoteModel {
     required this.color,
   });
 }
+// "flutter packages pub run build_runner build"
+//🥊🥊🥊🥊🥊 use this command to generate the file 
+//flutter pub run build_runner build
+
